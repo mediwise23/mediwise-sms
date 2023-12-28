e9027fa20b0adb7ac0aecce9d7f1859d3ba29137
jhondel <jhondeldelconacaranay@gmail.com>
2023-12-29 01:30:43 +0800

    update register api

---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,8 @@
+import { withAuth } from "@/lib/auth";
+import { getUserById } from "@/service/user";
+import { NextResponse } from "next/server";
+
+export const GET = withAuth(async ({ session }) => {
+  const user = await getUserById({ id: session.user.id });
+  return NextResponse.json(user);
+});
