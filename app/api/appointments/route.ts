0a4044e17aba2f9ee2bbac0a4ef7d72ffa9675e2
jhondel <jhondeldelconacaranay@gmail.com>
2023-12-30 00:53:56 +0800

    update appointment api

--- app/api/appointments/route.ts
+++ app/api/appointments/route.ts
@@ -1,3 +1,4 @@
+import { withAuth } from "@/lib/auth";
 import {
   AppointmentGetQuerySchema,
   CreateAppointmentSchema,
@@ -31,34 +32,42 @@ export async function GET(req: NextRequest, { params }: { params: {} }) {
   }
 }
 
-export async function POST(req: NextRequest, { params }: { params: {} }) {
-  try {
-    const body = await CreateAppointmentSchema.safeParseAsync(await req.json());
-
-    if (!body.success) {
-      return NextResponse.json(
-        {
-          errors: body.error.flatten().fieldErrors,
-          message: "Invalid body parameters",
-        },
-        { status: 400 }
+export const POST = withAuth(
+  async ({ req, session }) => {
+    try {
+      const body = await CreateAppointmentSchema.safeParseAsync(
+        await req.json()
       );
+
+      if (!body.success) {
+        return NextResponse.json(
+          {
+            errors: body.error.flatten().fieldErrors,
+            message: "Invalid body parameters",
+          },
+          { status: 400 }
+        );
+      }
+
+      const { title, doctorId, patientId, date, status, image_path } =
+        body.data;
+
+      const appointment = await createAppointment({
+        title,
+        doctorId,
+        patientId,
+        date,
+        status,
+        image_path,
+      });
+
+      return NextResponse.json(appointment, { status: 201 });
+    } catch (error) {
+      console.log("[APPOINMENT_POST]", error);
+      return new NextResponse("Internal error", { status: 500 });
     }
-
-    const { title, doctorId, patientId, date, status, image_path } = body.data;
-
-    const appointment = await createAppointment({
-      title,
-      doctorId,
-      patientId,
-      date,
-      status,
-      image_path,
-    });
-
-    return NextResponse.json(appointment, { status: 201 });
-  } catch (error) {
-    console.log("[APPOINMENT_POST]", error);
-    return new NextResponse("Internal error", { status: 500 });
+  },
+  {
+    requiredRole: ["ADMIN", "DOCTOR", "PATIENT"],
   }
-}
+);
