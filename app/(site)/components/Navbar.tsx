128e878b9820d7f6a05bd6d8b7107eb93d17dc3b
jhondel <jhondeldelconacaranay@gmail.com>
2024-01-09 15:44:57 +0800

    update landing page mobile responsive and dark theme

--- app/(site)/components/Navbar.tsx
+++ app/(site)/components/Navbar.tsx
@@ -1,22 +1,53 @@
-"use client"
+"use client";
+import { ThemeModeToggle } from "@/components/ThemeModeToggle";
 import { Button } from "@/components/ui/button";
 import { useModal } from "@/hooks/useModalStore";
 import Link from "next/link";
 import React from "react";
+import MobileLinks from "./MobileLinks";
 
 const Navbar = () => {
-  const { onOpen } = useModal()
+  const { onOpen } = useModal();
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 flex items-center justify-center w-full gap-x-5 max-h-[90px]">
-      <div className="flex w-[70%] items-center justify-between">
-      <img src="/images/bhaLogo.png" className="w-[90px]" alt="" />
-      <ul className="flex items-center gap-x-5">
-          <Link  href="#home" className="px-5 py-3 font-semibold">Home</Link>
-          <Link  href="#about-us" className="px-5 py-3 font-semibold">About us</Link>
-          <Link  href="#guidelines" className="px-5 py-3 font-semibold">Guidelines</Link>
-          <Link href="#contact-us" className="px-5 py-3 font-semibold">Contact Us</Link>
-      </ul>
-      <Button variant={'default'} onClick={() => onOpen('mediwiseLogin')} className="rounded-xl">Login</Button>
+    <nav className="sticky top-0 bg-white border-gray-200 dark:bg-gray-900 flex items-center justify-center w-full gap-x-5 max-h-[90px]">
+      <div className="flex w-full px-3 md:w-[70%] items-center justify-between">
+        <img
+          src="/images/bhaLogo.png"
+          className="w-[70px] md:w-[90px] "
+          alt=""
+        />
+        <ul className="flex items-center gap-x-1 md:gap-x-5 text-sm">
+          <Link href="#home" className="px-5 py-3 font-semibold">
+            Home
+          </Link>
+          <Link
+            href="#about-us"
+            className="hidden md:block px-5 py-3 font-semibold"
+          >
+            About us
+          </Link>
+          <Link
+            href="#guidelines"
+            className="hidden md:block px-5 py-3 font-semibold"
+          >
+            Guidelines
+          </Link>
+          <Link
+            href="#contact-us"
+            className="hidden md:block px-5 py-3 font-semibold"
+          >
+            Contact Us
+          </Link>
+          <MobileLinks />
+          <ThemeModeToggle />
+        </ul>
+        <Button
+          variant={"default"}
+          onClick={() => onOpen("mediwiseLogin")}
+          className="rounded-xl"
+        >
+          Login
+        </Button>
       </div>
     </nav>
   );
--- app/(site)/mediwise/(routes)/page.tsx
+++ app/(site)/mediwise/(routes)/page.tsx
@@ -6,23 +6,22 @@ import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
 const page = async () => {
-
   const session = await getSession();
   if (session?.user) {
     return redirect(`/mediwise/${session.user.role.toLowerCase()}`);
   }
 
   return (
-    <div className="flex flex-col  w-full mx-auto items-center gap-y-40">
+    <div className="flex flex-col  w-full mx-auto items-center gap-y-32 md:gap-y-40 dark:bg-dark">
       <Navbar />
       <section
-        className="w-full flex justify-center p-10 h-[700px] bg-zinc-50"
+        className="w-full flex justify-center p-5 md:p-10 h-fit md:h-[700px] bg-[#c5ebd3] dark:bg-gray-800"
         id="home"
       >
-        <div className=" w-[75%] flex justify-between gap-x-10 items-center clip-path:polygon(0%_0%,100%_0%,100%_100%)">
-          <img src="/images/Clogo.png" alt="" />
-          <div className="flex flex-col w-[60%] gap-y-10  ">
-            <h1 className="font-bold text-[40px] uppercase">
+        <div className="w-[95%] md:w-[75%] flex flex-col md:flex-row justify-start md:justify-between md:gap-x-10 items-center clip-path:polygon(0%_0%,100%_0%,100%_100%) pt-5">
+          <img src="/images/Clogo.png" alt="" className="w-32 md:w-56" />
+          <div className="flex flex-col md:w-[60%] gap-y-10  ">
+            <h1 className="font-bold text-[30px] lg:text-[40px] uppercase">
               A path to a better health
             </h1>
             <p className="text-zinc-500 text-md">
@@ -43,16 +42,16 @@ const page = async () => {
       </section>
       <section
         id="about-us"
-        className="w-full flex justify-center p-10 h-[700px] "
+        className="w-full flex justify-center p-5 md:p-10 h-fit md:h-[700px] bg-[#c5ebd3] dark:bg-gray-800"
       >
-        <div className=" w-[75%] flex flex-row-reverse gap-x-10 items-center">
+        <div className="w-[95%] md:w-[75%] flex flex-col md:flex-row-reverse gap-x-10 items-center">
           <img
             src="/images/BGPNU.jpeg"
-            className=" w-[50%] h-[400px] object-cover rounded-md"
+            className="w-full md:w-[50%] h-[400px] object-cover rounded-md"
             alt=""
           />
-          <div className="flex flex-col w-[50%] gap-y-10">
-            <h1 className="font-bold text-[40px] uppercase">
+          <div className="flex flex-col w-full md:w-[50%] gap-y-10">
+            <h1 className="text-center md:text-left font-bold text-[40px] uppercase">
               Find Out A Little More About Us
             </h1>
             <p className="text-zinc-500 text-md">
@@ -75,23 +74,23 @@ const page = async () => {
         </div>
       </section>
 
-      <section className=" w-[75%] flex flex-col " id="guidelines">
+      <section className="w-[75%] flex flex-col " id="guidelines">
         <h1 className="text-center text-[25px] font-semibold my-20 uppercase">
           A Few Guidelines On How To Use It
         </h1>
 
-        <div className="flex gap-x-20">
-          <div className="shadow-lg p-10 text-center font-semibold rounded-xl">
+        <div className="flex flex-col md:flex-row gap-x-20 gap-y-10">
+          <div className="shadow-2xl border-[.5px] p-10 text-center font-semibold rounded-xl dark:bg-gray-800">
             <h2>Login/Signup</h2>
             <img src="/images/1.png" alt="" />
           </div>
 
-          <div className="shadow-lg p-10 text-center font-semibold rounded-xl">
+          <div className="shadow-2xl border-[.5px] p-10 text-center font-semibold rounded-xl dark:bg-gray-800">
             <h2>Make an Appointment</h2>
             <img src="/images/2.png" alt="" />
           </div>
 
-          <div className="shadow-lg p-10 text-center font-semibold rounded-xl">
+          <div className="shadow-2xl border-[.5px] p-10 text-center font-semibold rounded-xl dark:bg-gray-800">
             <h2>Go to your local Barangay.</h2>
             <img src="/images/3.png" alt="" />
           </div>
@@ -99,7 +98,7 @@ const page = async () => {
       </section>
 
       <section
-        className="bg-[#c5ebd3] w-full p-10 h-[600px] flex items-center relative"
+        className=" w-full p-10 h-[600px] flex items-center relative bg-[#c5ebd3] dark:bg-gray-800"
         id="contact-us"
         style={{
           clipPath: "polygon(0 0, 100% 3%, 100% 100%, 0 98%)",
@@ -107,15 +106,14 @@ const page = async () => {
       >
         <img
           src="/images/contact2.png"
-          className="absolute top-0 left-0  w-[500px]"
+          className="absolute top-0 left-0 w-[200px] md:w-[300px] lg:w-[500px]"
           alt=""
         />
         <img
           src="/images/contact4.png"
-          className="absolute bottom-0 right-0 w-[500px]"
-          alt=""
+          className="absolute bottom-0 right-0 w-[200px] md:w-[300px] lg:w-[500px]"
         />
-        <div className="flex flex-col gap-y-10 w-[50%] mx-auto">
+        <div className="flex flex-col gap-y-10 w-[90%] md:w-[50%] mx-auto z-10 bg-black bg-opacity-30 p-10 rounded-xl">
           <h1 className="text-3xl uppercase text-center">
             C o n t a c t &nbsp; U s
           </h1>
@@ -132,7 +130,7 @@ const page = async () => {
 
             <div className="Phone">
               <h3 className="font-semibold">Email</h3>
-              <p>Mediwise@email.com</p>
+              <p className="drop-shadow-2xl">Mediwise@email.com</p>
             </div>
           </div>
         </div>
