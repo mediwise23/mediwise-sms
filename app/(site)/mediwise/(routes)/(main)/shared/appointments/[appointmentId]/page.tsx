30cbb0347b96de780e1dfacbde496c4e6a7a9bf8
AndroPyro28 <menandroeugenio1028@gmail.com>
2024-01-16 02:16:21 +0800

    appointment detail

---
+++ app/(site)/mediwise/(routes)/(main)/shared/appointments/[appointmentId]/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import AppoinmentsDetailClient from './components/AppoinmentsDetailClient'
+import { getSession } from '@/lib/auth';
+import { redirect } from 'next/navigation';
+import { getUserById } from '@/service/user';
+import { QueryClient } from '@tanstack/react-query';
+
+const page = async () => {
+
+    const session = await getSession();
+  if (!session?.user) {
+    return redirect("/");
+  }
+  const currentUser = await getUserById({ id: session.user.id });
+
+  if (!currentUser) {
+    return redirect("/");
+  }
+
+  const queryClient = new QueryClient();
+
+  return (
+    <div className='p-10'>
+        <AppoinmentsDetailClient currentUser={currentUser} />
+    </div>
+  )
+}
+
+export default page
