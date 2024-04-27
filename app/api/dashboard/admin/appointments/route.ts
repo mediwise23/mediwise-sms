24223e077043b4ae8ec675b20da0663c74bedc49
AndroPyro28 <menandroeugenio1028@gmail.com>
2024-04-27 09:22:52 -0400

    save

--- app/api/dashboard/admin/appointments/route.ts
+++ app/api/dashboard/admin/appointments/route.ts
@@ -64,7 +64,7 @@ export const GET = withAuth(async ({ req, session }) => {
       where: {
         barangayId: session.user.barangayId ?? undefined,
         AND: {
-          createdAt: {
+          date: {
             gte: new Date(selectedYear, i, 1),
             lt: new Date(selectedYear, i + 1, 1),
           },
